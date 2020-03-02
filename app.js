6533ede378f54df2b4ec8fe66b41b4a9a7d38e6c
meta-434 <ach1@vt.edu>
2020-03-02 15:44:29 -0500

    wip 7
    submission 1

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const { isWebUri } = require('valid-url');
 const helmet = require('helmet');
 const { v4: uuid } = require('uuid');
+const logger = require('./logger');
 const store = require('./store');
 
 
@@ -15,26 +17,49 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
-
 app.get('/bookmarks/:id', (req, res) => {
-    // indiv bookmark view.
-})
+    const get_id = req.params.id;
+
+    const bookmark = store.bookmarks.find(bkmrk => bkmrk.id == get_id);
+    console.log('matched bookmark: ', bookmark);
+    if (!bookmark) {
+        return res
+            .status(404)
+            .send('Bookmark Not Found')
+    }
+    res.json(bookmark)
+});
+
+app.get('/bookmarks', (req, res) => {
+    res.status(200).json(store.bookmarks);
+});
 
 app.post('/bookmarks', (req, res) => {
-    const { title, url, description, rating} = req.body;
-
-    const bookmark = { id: uuid(), title, url, description, rating };
-
+    for (const field of ['title', 'url', 'rating']) {
+        if (!req.body[field]) {
+            logger.error(`${field} is required`);
+            return res.status(400).send(`'${field}' is required`)
+        }
+    }
+    const { title, url, description, rating } = req.body;
+    if (parseInt(rating, 10) < 1 || parseInt(rating, 10) > 5) {
+        logger.error(`rating of ${rating} is invalid, must be greater than 0 and less than 6`);
+        return res.status(400).send(`invalid rating ${rating}. rating must be 1-5`);
+    }
+    if (!isWebUri(url)) {
+        logger.error(`invalid url ${url}`);
+        return res.status(400).send(`invalid url ${url}. must be a valid url.`)
+    }
+    const bookmark = { id: uuid(), title, url, description, rating: parseInt(rating, 10) };
     store.bookmarks.push(bookmark);
-    console.log(store);
     res.status(201).json(bookmark);
 });
 
 app.delete('/bookmarks/:id', (req, res) => {
-    console.log('req.params::::', req.params);
+    const delete_id = req.params.id;
+    const del_idx = store.bookmarks.findIndex(bkmrk => bkmrk.id === delete_id);
+    store.bookmarks.splice(del_idx, 1);
+    res.status(204).send(`deleted: ${delete_id}`);
 });
 
 app.use((error, req, res, next) => {
